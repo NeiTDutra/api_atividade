1b1542dc6f43902cfff46e49d075e0ff86253972
unknown <nei.thomass@gmail.com>
2020-06-08 22:04:55 -0300

    Criado arquivo de classe modelo para sqlite

---
+++ models.py
@@ -0,0 +1,13 @@
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+engine = create_engine('sqlite:///atividades.db', convert_unicode=True)
+db_session = scoped_session(sessionmaker(autocommit=False, bind=engine))
+
+Base = declarative_base()
+Base.query = db_session.query_property()
+
+class Pessoa(Base):
+    __tablename__='pessoas'
+    id = Column(Integer)
